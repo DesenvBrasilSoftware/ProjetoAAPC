ae485bbfd126c196e92dfc76a8efe6cf6e7935b5
Caio Ícaro <99915224+CaioIOX@users.noreply.github.com>
2023-08-29 11:56:09 -0300

    Implementação do controller de medicamento e suas respectivas telas

---
+++ app/Models/Medicamento.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class Medicamento extends BaseModel
+{
+    protected $table = 'medicamento';
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EnfermidadeController;
+use App\Http\Controllers\MedicamentoController;
 
 
 /*
@@ -27,3 +28,10 @@ Route::post('/enfermidade.store', [EnfermidadeController::class, 'store']);
 Route::get('/enfermidade.delete.{i}', [EnfermidadeController::class, 'delete']);
 Route::get('/enfermidade.edit.{i}', [EnfermidadeController::class, 'edit']);
 
+// Medicamento
+Route::get('/medicamento.index', [MedicamentoController::class, 'index']);
+Route::get('/medicamento.create', [MedicamentoController::class, 'create']);
+Route::get('/medicamento.retrieveAll', [MedicamentoController::class, 'retrieveAll']);
+Route::post('/medicamento.store', [MedicamentoController::class, 'store']);
+Route::get('/medicamento.delete.{i}', [MedicamentoController::class, 'delete']);
+Route::get('/medicamento.edit.{i}', [MedicamentoController::class, 'edit']);
