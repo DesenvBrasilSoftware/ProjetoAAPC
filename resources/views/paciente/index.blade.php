a3a93863e36fbb6b61278d667f192a1d059bdfd8
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-10-18 09:26:53 -0300

    Update index.blade.php

--- resources/views/paciente/index.blade.php
+++ resources/views/paciente/index.blade.php
@@ -1,44 +1,35 @@
 @extends('template')
 @section('conteudo')
-    <div class="row">
-        <div class="col-12">
-
-            <div class="card">
-                <div class="card-body">
-                    <a type="button" href="/paciente.create" class="btn btn-primary">Incluir</a>
-
-                    <table id="dataTable" class="table table-stripped">
-                        <thead>
-                            <tr>
-                                <th>Alterar</th>
-                                <th>Excluir</th>
-                                <th>nome </th>
-                                <th>data nascimento</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($lista as $obj)
-                                <input type="hidden" id="id_{{ $obj->id }}" value="{{ $obj->id }}">
-                                <tr>
-                                    <td width="1%">
-                                        <a href="/paciente.edit.{{ $obj->id }}"><i class="fa fa-edit"></i></a>
-                                    </td>
-                                    <td width="1%">
-                                        <a href="/paciente.delete.{{ $obj->id }}""><i class="fa fa-trash"></i></a>
-                                    </td>
-                                    <td>
-                                        {{ $obj->nome }}
-                                    </td>
-                                    <td>
-                                        {{ $obj->data_nascimento }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <!-- /.card -->
-        </div>
-    </div>
+<div class="form-group">
+  <a type="button" href="/paciente.create" class="btn btn-primary">Incluir</a>
+</div>
+<table id="dataTable" class="table table-stripped">
+  <thead>
+    <tr>
+      <th>Alterar</th>
+      <th>Excluir</th>
+      <th>Nome</th>
+      <th>Data de nascimento</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($lista as $obj)
+    <input type="hidden" id="id_{{ $obj->id }}" value="{{ $obj->id }}">
+    <tr>
+      <td width="1%">
+        <a href="/paciente.edit.{{ $obj->id }}"><i class="fa fa-edit"></i></a>
+      </td>
+      <td width="1%">
+        <a href="/paciente.delete.{{ $obj->id }}"><i class="fa fa-trash"></i></a>
+      </td>
+      <td>
+        {{ $obj->nome }}
+      </td>
+      <td>
+        {{ $obj->data_nascimento }}
+      </td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
 @endsection
