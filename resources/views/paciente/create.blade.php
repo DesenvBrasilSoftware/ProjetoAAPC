f18835998f41fbd119bc1bd5d2e279dabc147358
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-10-18 09:50:42 -0300

    placeholer no create

--- resources/views/paciente/create.blade.php
+++ resources/views/paciente/create.blade.php
@@ -3,28 +3,30 @@
 <form class="needs-validation" novalidate id="form" action="/paciente.store" method="post">
   @csrf
   <div class="form-group">
-    <label for="nome">Nome</label>
-    <input type="text" name="nome" class="form-control" id="nome" maxlength="50"
-      value="{{ old('nome') }}" autofocus>
-  </div>
-  <div class="form-group">
-    <label for="data_nascimento">Data de nascimento:</label>
-    <input type="date" class="form-control" id="data_nascimento" name="data_nascimento">
-  </div>
-  <div class="form-group">
-    <label for="cpf">CPF</label>
-    <input type="text" name="cpf" class="form-control" id="cpf" maxlength="11"
-      value="{{ old('cpf') }}" autofocus oninput="this.value = this.value.replace(/[^0-9]/g, '');">
-  </div>
-  <div class="form-group">
-    <label for="rg">RG</label>
-    <input type="text" name="rg" class="form-control" id="rg" maxlength="20"
-      value="{{ old('rg') }}" autofocus oninput="this.value = this.value.replace(/[^0-9]/g, '');">
-  </div>
-  <div class="form-group">
-    <label for="data_cadastro">Data de cadastro:</label>
-    <input type="date" class="form-control" id="data_cadastro" name="data_cadastro">
-  </div>
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" class="form-control" id="nome" maxlength="50"
+        value="{{ old('nome') }}" autofocus placeholder="Insira seu nome">
+    </div>
+    <div class="form-group">
+        <label for="data_nascimento">Data de nascimento:</label>
+        <input type="date" class="form-control" id="data_nascimento" name="data_nascimento" placeholder="Insira sua data de nascimento">
+    </div>
+    <div class="form-group">
+        <label for="cpf">CPF:</label>
+        <input type="text" name="cpf" class="form-control" id="cpf" maxlength="11"
+        value="{{ old('cpf') }}" autofocus oninput="this.value = this.value.replace(/[^0-9]/g, '');"
+        placeholder="Informe o CPF">
+    </div>
+    <div class="form-group">
+        <label for="rg">RG:</label>
+        <input type="text" name "rg" class="form-control" id="rg" maxlength="20"
+        value="{{ old('rg') }}" autofocus oninput="this.value = this.value.replace(/[^0-9]/g, '');"
+        placeholder="Informe o RG">
+    </div>
+    <div class="form-group">
+        <label for="data_cadastro">Data de cadastro:</label>
+        <input type="date" class="form-control" id="data_cadastro" name="data_cadastro" placeholder="Insira a data de cadastro">
+    </div>
   <div class="form-group">
     <label for="sexo">Sexo</label><br>
     <label>
@@ -39,9 +41,10 @@
     </label>
   </div>
   <div class="form-group">
-    <label for="quantidade_filhos">Quantidade de filhos</label>
+    <label for="quantidade_filhos">Quantidade de filhos:</label>
     <input type="text" name="quantidade_filhos" class="form-control" id="quantidade_filhos"
-      value="{{ old('quantidade_filhos') }}" autofocus oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+        value="{{ old('quantidade_filhos') }}" autofocus oninput="this.value = this.value.replace(/[^0-9]/g,');"
+        placeholder="Informe a quantidade de filhos">
   </div>
   <div class="form-group">
     <label for="estado_civil">Estado civil</label><br>
@@ -73,78 +76,77 @@
   </div>
   <div class="form-group">
     <label for="conjuge">Conjuge</label>
-    <input type="text" name="conjuge" class="form-control" id="conjuge" maxlength="50"
-      value="{{ old('conjuge') }}" autofocus>
-  </div>
-  <div class="form-group">
+    <input type="text" name="conjuge" class="form-control" id="conjuge" maxlength="50" placeholder="Digite o nome do cônjuge" value="{{ old('conjuge') }}" autofocus>
+</div>
+
+<div class="form-group">
     <label for="escolaridade">Escolaridade</label><br>
     <label>
-    <input type="radio" name="escolaridade" id="ensinoMedio" value="0"
-    {{ old('escolaridade') == '0' ? 'checked' : '' }}>
-    Ensino médio
+        <input type="radio" name="escolaridade" id="ensinoMedio" value="0" {{ old('escolaridade') == '0' ? 'checked' : '' }}>
+        Ensino médio
     </label>
     <label>
-    <input type="radio" name="escolaridade" id="superior" value="1"
-    {{ old('escolaridade') == '1' ? 'checked' : '' }}>
-    Superior
+        <input type="radio" name="escolaridade" id="superior" value="1" {{ old('escolaridade') == '1' ? 'checked' : '' }}>
+        Superior
     </label>
     <label>
-    <input type="radio" name="escolaridade" id="mestrado" value="2"
-    {{ old('escolaridade') == '2' ? 'checked' : '' }}>
-    Mestrado
+        <input type="radio" name="escolaridade" id="mestrado" value="2" {{ old('escolaridade') == '2' ? 'checked' : '' }}>
+        Mestrado
     </label>
     <label>
-    <input type="radio" name="escolaridade" id="doutorado" value="3"
-    {{ old('escolaridade') == '3' ? 'checked' : '' }}>
-    Doutorado
+        <input type="radio" name="escolaridade" id="doutorado" value="3" {{ old('escolaridade') == '3' ? 'checked' : '' }}>
+        Doutorado
     </label>
-  </div>
-  <div class="form-group">
-    <label for="profissao">Profissão</label>
-    <input type="text" name="profissao" class="form-control" id="profissao" maxlength="45"
-      value="{{ old('profissao') }}" autofocus>
-  </div>
-  <div class="form-group">
-    <label for="renda_mensal">Renda mensal</label>
-    <input type="number" step="0.01" name="renda_mensal" class="form-control" id="renda_mensal"
-      value="{{ old('renda_mensal') }}" autofocus>
-  </div>
-  <div class="form-group">
-    <label for="observacao">Observação</label>
-    <input type="text" name="observacao" class="form-control" id="observacao"
-      value="{{ old('observacao') }}" autofocus>
-  </div>
-  <div class="form-group">
-    <label for="cep">CEP</label>
-    <input type="text" name="cep" class="form-control" id="cep" maxlength="8"
-      value="{{ old('cep') }}" autofocus oninput="this.value = this.value.replace(/[^0-9]/g, '');">
-  </div>
-  <div class="form-group">
-    <label for "logradouro">Logradouro</label>
-    <input type="text" name="logradouro" class="form-control" id="logradouro" maxlength="60"
-      value="{{ old('logradouro') }}" autofocus>
-  </div>
-  <div class="form-group">
-    <label for "numero">Número</label>
-    <input type="text" name="numero" class="form-control" id="numero" maxlength="6"
-      value="{{ old('numero') }}" autofocus oninput="this.value = this value.replace(/[^0-9]/g, '');">
-  </div>
-  <div class="form-group">
-    <label for "complemento">Complemento</label>
-    <input type="text" name="complemento" class="form-control" id="complemento" maxlength="45"
-      value="{{ old('complemento') }}" autofocus>
-  </div>
-  <div class="form-group">
-    <label for "ponto_referencia">Ponto de referência</label>
-    <input type="text" name="ponto_referencia" class="form-control" id="ponto_referencia" maxlength="45"
-      value="{{ old('ponto_referencia') }}" autofocus>
-  </div>
-  <div class="form-group">
-    <label for "bairro_id">Bairro</label>
-    <input type="text" name="bairro_id" class="form-control" id="bairro_id"
-      value="{{ old('bairro_id') }}" autofocus>
-  </div>
-  <button type="submit" class="btn btn-primary">Salvar</button>
-  <a type="button" href="/paciente.index" class="btn btn-warning">Fechar</a>
+</div>
+
+<div class="form-group">
+    <label for="profissao">Profissão:</label>
+    <input type="text" name="profissao" class="form-control" id="profissao" maxlength="45" placeholder="Digite a profissão" value="{{ old('profissao') }}" autofocus>
+</div>
+
+<div class="form-group">
+    <label for="renda_mensal">Renda mensal:</label>
+    <input type="number" step="0.01" name="renda_mensal" class="form-control" id="renda_mensal" placeholder="Digite a renda mensal" value="{{ old('renda_mensal') }}" autofocus>
+</div>
+
+<div class="form-group">
+    <label for="observacao">Observação:</label>
+    <input type="text" name="observacao" class="form-control" id="observacao" placeholder="Digite uma observação" value="{{ old('observacao') }}" autofocus>
+</div>
+
+<div class="form-group">
+    <label for="cep">CEP:</label>
+    <input type="text" name="cep" class="form-control" id="cep" maxlength="8" placeholder="Digite o CEP" value="{{ old('cep') }}" autofocus oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+</div>
+
+<div class="form-group">
+    <label for="logradouro">Logradouro:</label>
+    <input type="text" name="logradouro" class="form-control" id="logradouro" maxlength="60" placeholder="Digite o logradouro" value="{{ old('logradouro') }}" autofocus>
+</div>
+
+<div class="form-group">
+    <label for="numero">Número:</label>
+    <input type="text" name="numero" class="form-control" id="numero" maxlength="6" placeholder="Digite o número" value="{{ old('numero') }}" autofocus oninput="this.value = this.value.replace(/[^0-9]/g, '');">
+</div>
+
+<div class="form-group">
+    <label for="complemento">Complemento:</label>
+    <input type="text" name="complemento" class="form-control" id="complemento" maxlength="45" placeholder="Digite o complemento" value="{{ old('complemento') }}" autofocus>
+</div>
+
+<div class="form-group">
+    <label for="ponto_referencia">Ponto de referência:</label>
+    <input type="text" name="ponto_referencia" class="form-control" id="ponto_referencia" maxlength="45" placeholder="Digite um ponto de referência" value="{{ old('ponto_referencia') }}" autofocus>
+</div>
+
+<div class="form-group">
+    <label for="bairro_id">Bairro:</label>
+    <input type="text" name="bairro_id" class="form-control" id="bairro_id" placeholder="Digite o bairro" value="{{ old('bairro_id') }}" autofocus>
+</div>
+
+<div class="form-group">
+    <a type="button" href="/paciente.index" class="btn btn-warning">Fechar</a>
+    <button type="submit" class="btn btn-primary">Salvar</button>
+</div>
 </form>
 @endsection
