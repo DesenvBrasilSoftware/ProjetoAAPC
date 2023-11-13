09f04a0eeb3c17ba4581024c2af3e9d0b54a1b92
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-11-13 13:56:02 -0300

    Finalizando base do portal

--- resources/views/portal.blade.php
+++ resources/views/portal.blade.php
@@ -9,6 +9,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="/js/portal.js"></script>
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+
     <link href="/css/portal.css" rel="stylesheet" />
   </head>
   <body>
@@ -41,6 +43,32 @@
         </div>
       </div>
     </nav>
-    <div class="container-fluid"></div>
+    <div class="container-fluid">
+      <div class="row" style="margin: 2rem 0">
+        <div class="col d-flex justify-content-center mb-4">
+          <img width="360px" src="/images/card-doacao.png" alt="" />
+        </div>
+        <div class="col mx-auto">
+          <div class="qrcode-card">
+            <h1 class="text-center" style="margin-bottom: 0px;">Doe com <i style="color: #30b6a8;" class="fa-brands fa-pix"></i> PIX</h1>
+            <img width="90%" src="/images/qrcode-pix.png" alt="" />
+            <span class="text-center">Chave PIX: 05.363.115/0001-64</span>
+            <span class="text-center">Nome: AAPC</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1 mr-2">
+        <img style="filter: grayscale(100%);" width="32px"src="/favicon.png" alt="">
+      </a>
+      <span class="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li style="font-size: 2rem" class="ms-3"><a class="text-muted" href="#">Instagram <i class='bx bxl-instagram-alt' ></i></a></li>
+    </ul>
+  </footer>
   </body>
 </html>
